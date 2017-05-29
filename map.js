167db97f920f2071c8995a6cae3410856e524d78
David Schilling <davejs92@gmail.com>
2017-05-29 17:05:46 +0200

    Integrate server to get church data, with parse

--- map.js
+++ map.js
@@ -4,20 +4,18 @@ var churches = [
 ];
 
 function initMap() {
-	var gomaringen = {lat: 48.45246, lng: 9.0931113};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 10,
-		center: gomaringen
-	});
-	churches.forEach(function (church) {
-		var marker = new google.maps.Marker({
-			position: {lat: church.lat, lng: church.lng},
-			map: map,
-			title: church.name + ' in ' + church.city
-		});
-		marker.addListener('click', function() {
-			showDetails(church);
-		});
+	$.ajax({
+		type: 'GET',
+		url: 'https://parse.buddy.com/parse/classes/churches',
+		dataType: 'json',
+		headers: {
+			'X-Parse-Application-Id' : '430ddd14-5f2d-41a2-8a15-53c216e5d90b',
+			'X-Parse-REST-API-Key' : 'Kq8lTFun90NspXE7qfT9LlXbMAG2Kqaq'
+		},
+		success: function (data) {
+			churches = data.results;
+			drawMarkers();
+		}
 	});
 }
 
@@ -28,4 +26,24 @@ function showDetails(church) {
 	$details.append($header)
 	$details.append($('<div>').html('<a href="' + church.website +'">Zur Webseite</a>'));
 
+}
+
+function drawMarkers() {
+	var gomaringen = {lat: 48.45246, lng: 9.0931113};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 10,
+		center: gomaringen
+	});
+	churches.forEach(function (church) {
+		var markerOptions = {
+			position: {lat: church.location.latitude, lng: church.location.longitude},
+			map: map,
+			title: church.name + ' in ' + church.city
+		};
+		console.log(markerOptions);
+		var marker = new google.maps.Marker(markerOptions);
+		marker.addListener('click', function() {
+			showDetails(church);
+		});
+	});
 }
